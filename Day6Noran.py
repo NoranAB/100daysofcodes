276c15af23c61e302ac376028190c93fbbc58df1
Noran1 <noranoz41@gmail.com>
2019-08-27 03:27:06 +0300

    6day

---
+++ Day6Noran.py
@@ -0,0 +1,27 @@
+#Example1
+x = int(2) #x will be 1
+y = int(3.6) # y will be 2
+z = int("4") # z will be 3
+print (x)
+print(y)
+print(z)
+
+#Example2
+x = float(2) # x will be 1.0
+y = float (3.6) # y will be 3.6
+z = float ("4") # z will be 4.0
+n = float ("4.6") # n will be 4.6
+print (x)
+print(y)
+print(z)
+print(n)
+
+#Example3
+i = str ("sl") # i will be 'sl'
+j = str (4) # j will be '4'
+z = str (6.0) # z will be '6.0'
+print (i)
+print (j)
+print (z)
+
+
