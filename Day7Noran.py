4ce07fb097614c3a19f96afde255fe9327fa015f
Noran1 <noranoz41@gmail.com>
2019-08-27 04:07:47 +0300

    7day

---
+++ Day7Noran.py
@@ -0,0 +1,20 @@
+#Example1 python String
+print("Hello")
+print('Hi')
+
+#Example2 Assign String to a variable
+x = "Python"
+print(x)
+
+#Example3 Multiline Strings
+i= """Lorem ipsum dolor sit amet,
+consectetur adipisciing elite,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliua."""
+print(i)
+
+#Example4 Strings are Arrays
+x = "Hello, World!"
+print(x[1])
+y= "Hello, World!"
+print(y[2:5])
