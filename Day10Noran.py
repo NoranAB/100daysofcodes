05ae80c3b73ebb53ecc8471c047212618527ea9b
Noran1 <noranoz41@gmail.com>
2019-08-31 08:55:01 +0300

    10day

---
+++ Day10Noran.py
@@ -0,0 +1,29 @@
+#Python operators  العوامل في لغة البايثون
+
+#Example1  python arthimetic operators العوامل في العمليات الحسابيه
+i = 6
+j = 2
+print (i*j)
+x= 4
+y = 7
+print (x+y)
+f=8
+z=3
+print(f-z)
+r=9
+n=3
+print(9/3)
+
+#EXample2 python Assignment operators وامل اعطاء قيم للمتغيرات / تخصيص
+i = 6
+j /=2
+print(j)
+
+#Example3 python cpmpatsion operators عوامل مقارناتto values
+x = 6
+y = 2
+print(x<y)
+i= 8
+j= 9
+print (i<j)
+
