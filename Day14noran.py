9c77692798b2369ad8bdd7ba4c2f8a7fcdc13813
Noran1 <noranoz41@gmail.com>
2019-09-04 07:39:25 +0300

    14day

---
+++ Day14noran.py
@@ -0,0 +1,17 @@
+#Example1
+thislist = ["A", "S", "K", "N", "W", "R"]
+print (thislist[0:2])
+print(thislist[2:6])
+
+#Example2
+mylist = ["March", "September", "June"]
+if "March" in mylist:
+    print("Yes, 'March' is in the month list")
+#Example3
+z=["Python"]*4
+print(z)
+#Example4
+i= ["July", "June", "april"]
+j=["Rayn", "John", "Jasmin"]
+x= i+j
+print(x)
