926617473a9aac9aa1fba3b8e790456fa7c9bd0a
Noran1 <noranoz41@gmail.com>
2019-09-04 08:11:47 +0300

    16day

---
+++ Day16noran.py
@@ -0,0 +1,20 @@
+#Python Tuples
+#Example1
+thistuple = ("blue", "black","pink")
+print (thistuple)
+z = ()
+print(z)
+i = (6, )
+print(i)
+j = (2,4.9,6,8.2)
+print(j)
+tuple = ("Reem", 2.2, 6, "Python", "بايثون")
+print(tuple)
+#access tuple items
+tuples = ("cherry", "green apple", "orange")
+print(tuples[0])
+y = ("March", "June", "September")
+for x in y:
+    print(x)
+mytuple = ("Javascript", "Python", "Swift")
+print(mytuple [0:2])
