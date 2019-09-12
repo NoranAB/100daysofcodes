01673436008905b93c49e3f5bd3a8e5617645d87
Noran1 <noranoz41@gmail.com>
2019-09-12 04:27:16 +0300

    15day

---
+++ noran15day.py
@@ -0,0 +1,48 @@
+#Data structure - python lists3 - list methods
+thislist = ["apple", "banana", "cherry"]
+mylist = list (thislist)
+print (mylist)
+
+#Example1 List Length - len()
+z = ["python", "javascript", "PHP"]
+print (len(z))
+
+#Example2 Add Items to the end of the list - append()
+x = ["swift", "Javascript", "MicroBit"]
+x.append("Python")
+print(x)
+
+#Example3 add Items at the specified index, insert()
+i = ["swift", "PHP", "MicroBit"]
+i.insert(0, "Python")
+print(i)
+
+#Example4 remove()
+thislist = ["blue", "black", "python"]
+thislist.remove("python")
+print(thislist)
+
+#Example5 pop()
+list = ["Python", "Javascript", "Swift"]
+list.pop()
+print(list)
+list.pop(1)
+print(list)
+
+#Example6 clear()
+j = ["cherry", "apple", "orange"]
+j.clear()
+print(j)
+
+#EXample7 Copy a List
+z = ["Python", "Javascript","Swift"]
+x=z.copy()
+z.pop(0)
+print(x)
+print(z)
+
+x = ["Python", "Javascript","Swift"]
+y=z
+x.pop(0)
+print(y)
+print(x)
