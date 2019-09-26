0d5dba087c6e08b72a461c10eed34674e68e0f3c
Noran1 <noranoz41@gmail.com>
2019-09-26 04:11:58 +0300

    37day

---
+++ Day37noran.py
@@ -0,0 +1,11 @@
+#Python Arrays
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+#Access the elements of an array
+car = ["BMW", "Volvo", "Ford"]
+x = car[0]
+print(x)
+car [1] = "KIA"
+print(car)
+y = len(car) #length of an array
+print(y)
