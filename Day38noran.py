3ecddb0ef89cd818f9ce7508cc1547a29fa45fdb
Noran1 <noranoz41@gmail.com>
2019-09-26 05:10:41 +0300

    38day

---
+++ Day38noran.py
@@ -0,0 +1,15 @@
+#Python Arrays2
+#Looping Array Elements
+colors = ["Blue", "White", "Black", "Red"]
+for z in colors:
+    print(z)
+#Adding Array Elements used append()
+Languages = ["Javascript", "PHP", "Python"]
+Languages.append("Microbit")
+print(Languages)
+#Removing Array Elements used pop() or remove()
+fruits = ["Blueberry", "cherry", "blackberry", "rasepberry"]
+fruits.pop(1)
+print(fruits)
+fruits.remove("blackberry")
+print(fruits)
